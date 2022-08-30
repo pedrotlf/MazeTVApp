fa41eb8087befb662b3e3679da8fc9db11da61d3
Pedro Flores <pedrodroke@gmail.com>
2022-08-29 23:22:34 -0300

    Fixing air days not being shown (was showing only air time)

--- app/src/main/java/br/com/pedrotlf/mazetvapp/presentation/show_details/ShowDetailsFragment.kt
+++ app/src/main/java/br/com/pedrotlf/mazetvapp/presentation/show_details/ShowDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -66,6 +67,7 @@ class ShowDetailsFragment: BaseFragment() {
         binding.apply {
             Glide.with(this@ShowDetailsFragment).load(show.posterImage).into(ivShowImage)
 
+            // Formating genres as "Genre1, Genre2, Genre3"
             var genresText = ""
             show.genres.forEachIndexed { i, txt ->
                 genresText += txt
@@ -73,7 +75,21 @@ class ShowDetailsFragment: BaseFragment() {
                     genresText += ", "
             }
             tvShowDetailsGenres.text = genresText
-            tvShowDetailsAirTimeText.text = show.airTime
+
+            tvShowDetailsAirTime.isVisible = true
+            // Formating air days as ex.: "Sunday, Monday and Tuesday at 12:00"
+            var airDaysText = show.airDays.firstOrNull().orEmpty()
+            show.airDays.forEachIndexed { i, day ->
+                if(i != 0){
+                    airDaysText += if(i != show.airDays.lastIndex)
+                        ", $day"
+                    else
+                        " and $day"
+                }
+            }
+            airDaysText += " at ${show.airTime}"
+            tvShowDetailsAirTimeText.text = airDaysText
+
             tvShowDetailsSummary.text = HtmlCompat.fromHtml(show.summary, HtmlCompat.FROM_HTML_MODE_LEGACY)
         }
     }
